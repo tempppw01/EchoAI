8bbb7c458a4377d6fdc84cc0de1b29f07471a9a0
tempppw01 <temppw01@gmail.com>
2026-02-17 20:27:49 +0800

    fix: forward input ref for react-hook-form registration

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,6 @@
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import { cn } from '@/lib/utils';
 
-export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={cn('w-full rounded-md border bg-card px-3 py-2 text-sm', props.className)} />;
-}
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(function Input(props, ref) {
+  return <input ref={ref} {...props} className={cn('w-full rounded-md border bg-card px-3 py-2 text-sm', props.className)} />;
+});
